9601614ce52bdd7b7180e81fec46d24530a1ff87
Konstantin Schneider <ks@konstantin-schneider.eu>
2021-11-02 13:46:40 +0000

    Create skeleton of all planned core functions.

---
+++ R/log_progress.R
@@ -0,0 +1,9 @@
+#' Get details for the next training.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_next_training <- function() {
+
+}
---
+++ R/update_plan.R
@@ -0,0 +1,29 @@
+#' Update a workout-plan
+#'
+#' @return
+#' @export
+#'
+#' @examples
+update_plan <- function() {
+
+}
+
+#' Add a training to a workout-plan
+#'
+#' @return
+#' @export
+#'
+#' @examples
+add_training <- function() {
+
+}
+
+#' Deload weights for next training.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+deload <- function() {
+  print("sad...")
+}
